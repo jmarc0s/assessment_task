0c6d5e01de173ac13d9c9b34a67849e0e99e5901
jmarc0s <joaomarcoscacula88@gmail.com>
2023-11-22 12:07:56 -0300

    Inicio

---
+++ src/main/java/br/com/jmarcos/assessment_task/AssessmentTaskApplication.java
@@ -0,0 +1,13 @@
+package br.com.jmarcos.assessment_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssessmentTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssessmentTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jmarcos/assessment_task/AssessmentTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jmarcos.assessment_task;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssessmentTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
